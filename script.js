10820eb12a14ddd1e34bfb4f55ba67ed1c4debf1
Josareta <126685687+Josareta@users.noreply.github.com>
2023-04-19 21:34:44 +0200

    Add files via upload
    El archivo de Html, añade el atributo UTF8 de codificación de caracteres. El archivo Script.js, añade un segundo botón para cambiar de color el fondo de pantalla a azul.

---
+++ script.js
@@ -0,0 +1,12 @@
+const boton_1 = document.getElementById('cambiarColor_1')
+
+boton_1.addEventListener('click', () => {
+  const body = document.querySelector('body')
+  body.style.backgroundColor = 'yellow'
+})
+const boton_2 = document.getElementById('cambiarColor_2')
+
+boton_2.addEventListener('click', () => {
+  const body = document.querySelector('body')
+  body.style.backgroundColor = 'blue'
+})
